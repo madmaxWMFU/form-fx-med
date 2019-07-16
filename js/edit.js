5d8958dc63902686f1877374dcee3347e3646d8a
madmaxWMFU <yasmaks@gmail.com>
2019-07-16 16:56:24 +0300

    update files edit

--- js/edit.js
+++ js/edit.js
@@ -1,4 +1,124 @@
-document.addEventListener("DOMContentLoaded", function() {
+const main = document.querySelector(".user-list");
+
+$(document).on("click", ".user-list", function(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	$.ajax({
+	    url: "editUserInfo.php", 
+	    data: {type: 2, idUser: $(e.target).data("id")}, 
+	    type: 'POST', 
+      	success: function(msg) {
+			setUserInfo(JSON.parse(msg)[0]);
+      	} 		
+	})
+})
+
+$(document).on("click", ".editClient", function(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	$.ajax({
+	    url: "editUserInfo.php", 
+	    data: {type: 3, info: getNewUserInfo()}, 
+	    type: 'POST', 
+      	success: function(data) {
+			console.log(JSON.parse(data));
+			location.reload();
+      	} 		
+	})
+})
+
+$(document).on("click", ".singOut", function(){
+	window.sessionStorage.removeItem('auth');
+	window.open('../index.html', "_self");
+})
+
+function setUserInfo(info) {
+	$("#hospital").val(info.hospital);
+	$("#surnameDoctor").val(info.surnameDoctor);
+	$("#region").val(info.region);
+	$("#phoneDoctor").val(info.phoneDoctor);
+	$("#emailDoctor").val(info.emailDoctor);
+	$("#dateExamination").val(info.dateExamination);
+	$("#surname").val(info.surnameUser);
+	$("#name").val(info.nameUser);
+	$("#lastName").val(info.lastNameUser);
+	$("#dateBirthday").val(info.dateBirthday);
+	$("input[value='"+info.gender+"']").prop('checked', true);
+	$("#email").val(info.email);
+	$("#phone").val(info.phone);
+	setRadioGroup("heredityRadios", info.heredity);
+	setRadioGroup("smokyRadios", info.smoky);
+	setRadioGroup("wokrRadios", info.work);
+	setRadioGroup("alergoRadios1", info.alergos1);
+	setRadioGroup("alergoSeasonRadios1", info.alergoSeason1);
+	setRadioGroup("alergoYearRadios1", info.alergoYear1);
+	setRadioGroup("alergoRadios2", info.alergos2);
+	setRadioGroup("alergoSeasonRadios2", info.alergoSeason2);
+	setRadioGroup("alergoYearRadios2", info.alergoYear2);
+	setRadioGroup("alergoRadios3", info.alergos3);
+	setRadioGroup("alergoSeasonRadios3", info.alergoSeason3);
+	setRadioGroup("alergoYearRadios3", info.alergoYear3);
+	setRadioGroup("alergoRadios4", info.alergos4);
+	setRadioGroup("alergoRadios5", info.alergos5);
+	setRadioGroup("alergoRadios6", info.alergos6);
+	setRadioGroup("alergoRadios7", info.alergos7);
+	setRadioGroup("alergoRadios8", info.alergos8);
+	setRadioGroup("alergoRadios9", info.alergos9);
+	setRadioGroup("alergoRadios10", info.alergos10);
+	$(".editClient").attr("data-id", info.idUser);
+}
+
+function setRadioGroup(vRadioObj, vValue) {
+    var radios = document.getElementsByName(vRadioObj);
+    for (var j = 0; j < radios.length; j++) {
+        if (radios[j].value == vValue) {
+            radios[j].checked = true;
+            break;
+        }
+    }
+}
+
+function getNewUserInfo() {
+	return {
+		"surname": $("#surname").val(),
+		"name": $("#name").val(),
+		"lastName": $("#lastName").val(),
+		"dateBirthday": $("#dateBirthday").val(),
+		"gender": $("[name='genderRadios']:checked").val(),
+		"email": $("#email").val(),
+		"phone": $("#phone").val(),
+		"address": $("#address").val(),
+		"region": $("#region").val(),
+		"heredity": $("[name='heredityRadios']:checked").val(),
+		"smoky": $("[name='smokyRadios']:checked").val(),
+		"work": $("[name='wokrRadios']:checked").val(),
+		"alergo1": $("[name='alergoRadios1']:checked").val(),
+		"alergoSeason1": $("[name='alergoSeasonRadios1']:checked").val(),
+		"alergoYear1": $("[name='alergoYearRadios1']:checked").val(),
+		"alergo2": $("[name='alergoRadios2']:checked").val(),
+		"alergoSeason2": $("[name='alergoSeasonRadios2']:checked").val(),
+		"alergoYear2": $("[name='alergoYearRadios2']:checked").val(),
+		"alergo3": $("[name='alergoRadios3']:checked").val(),
+		"alergoSeason3": $("[name='alergoSeasonRadios3']:checked").val(),
+		"alergoYear3": $("[name='alergoYearRadios3']:checked").val(),
+		"alergo4": $("[name='alergoRadios4']:checked").val(),
+		"alergo5": $("[name='alergoRadios5']:checked").val(),
+		"alergo6": $("[name='alergoRadios6']:checked").val(),
+		"alergo7": $("[name='alergoRadios7']:checked").val(),
+		"alergo8": $("[name='alergoRadios8']:checked").val(),
+		"alergo9": $("[name='alergoRadios9']:checked").val(),
+		"alergo10": $("[name='alergoRadios10']:checked").val(),
+		"hospital": $("#hospital").val(),
+		"surnameDoctor": $("#surnameDoctor").val(),
+		"phoneDoctor": $("#phoneDoctor").val(),
+		"emailDoctor": $("#emailDoctor").val(),
+		"dateExamination": $("#dateExamination").val(),
+		"checkItem": Math.floor(Date.now()),
+		"idUser": $(".editClient").data("id")
+	};
+}
+
+function init() {
 	if(!window.sessionStorage.getItem('auth')){
 		window.open('../index.html', "_self");
 	} else {
@@ -7,44 +127,13 @@ document.addEventListener("DOMContentLoaded", function() {
 		    data: {type: 1}, 
 		    type: 'POST', 
 	      	success: function(msg) {
-				arr = JSON.parse(msg);
-				$(arr).each(function(key, val) {
-					$(`<div class="user-list-item" data-id="${val['idUser']}">${val['name']}</div>`).appendTo(".user-list")
+				JSON.parse(msg).forEach(function(val) {
+					main.insertAdjacentHTML("beforeEnd", `<div class="user-list-item" data-id="${val['idUser']}">${val['name']}</div>`);
 				})
 	      	} 		
 		})
 	}
-})
+}
 
-// $(document).on("click", ".addClient", function(e) {
-// 	e.stopPropagation();
-// 	e.preventDefault();
-// 	if(checkInputs()){
-// 		$.ajax({
-// 		    url: "setData.php", 
-// 		    dataType: 'json',
-// 		    cache: false,
-// 		    contentType: false,
-// 		    processData: false,
-// 		    data: getData(), 
-// 		    type: 'POST', 
-// 	      	success: function(msg) {
-// 				clearInputs();
-// 	      	} 		
-// 		})
-// 	} else {
-// 		alert("Заповніть всі данні!");
-// 	}
-// })
+init();
 
-$(document).on("click", ".singOut", function(){
-	window.sessionStorage.removeItem('auth');
-	window.open('../index.html', "_self");
-})
-
-// function init() {
-// 	if(!window.sessionStorage.getItem('auth'))
-// 		window.open('../index.html', "_self");
-// }
-
-// init();
--- pages/editUserInfo.php
+++ pages/editUserInfo.php
@@ -6,13 +6,13 @@
 
 	switch ($_POST['type']) {
 		case 1:
-			$allPacients = 'SELECT id_user, surname_user, name_user, last_name_user FROM user_info';
+			$allPacients = 'SELECT id_user, surname_user, name_user, last_name_user FROM user_info ORDER BY surname_user';
 			if($result = $mysqli->query($allPacients)) {
 				$row = $result->fetch_assoc();
 				$usersArray = array();
 				do{
 					array_push($usersArray, Array('idUser' => $row['id_user'], 'name' => $row['surname_user'].' '.$row['name_user'].' '.$row['last_name_user']));
-				}while ($row = $result->fetch_assoc());
+				} while($row = $result->fetch_assoc());
 				echo json_encode($usersArray);
 			} else {
 				printError($mysqli->error); 
@@ -22,24 +22,97 @@
 		
 		case 2:
 			$onePacients = 'SELECT * FROM user_info WHERE id_user = '.$_POST['idUser'];
-			if($mysqli->query($onePacients)) {
-				echo json_encode(array("msg" => ""));
+			if($result = $mysqli->query($onePacients)) {
+				$row = $result->fetch_assoc();
+				$userArray = array();
+				do {
+					array_push($userArray, array("idUser" => $row['id_user'],
+												"surnameUser" => $row['surname_user'],
+												"nameUser" => $row['name_user'],
+												"lastNameUser" => $row['last_name_user'],
+												"dateBirthday" => $row['date_birthday'],
+												"gender" => $row['gender'],
+												"email" => $row['email'],
+												"phone" => $row['phone'],
+												"address" => $row['address'],
+												"region" => $row['region'],
+												"heredity" => $row['heredity'],
+												"smoky" => $row['smoky'],
+												"work" => $row['work'],
+												"alergos1" => $row['alergos1'],
+												"alergoSeason1" => $row['alergoSeason1'],
+												"alergoYear1" => $row['alergoYear1'],
+												"alergos2" => $row['alergos2'],
+												"alergoSeason2" => $row['alergoSeason2'],
+												"alergoYear2" => $row['alergoYear2'],
+												"alergos3" => $row['alergos3'],
+												"alergoSeason3" => $row['alergoSeason3'],
+												"alergoYear3" => $row['alergoYear3'],
+												"alergos4" => $row['alergos4'],
+												"alergos5" => $row['alergos5'],
+												"alergos6" => $row['alergos6'],
+												"alergos7" => $row['alergos7'],
+												"alergos8" => $row['alergos8'],
+												"alergos9" => $row['alergos9'],
+												"alergos10" => $row['alergos10'],
+												"hospital" => $row['hospital'],
+												"surnameDoctor" => $row['surnameDoctor'],
+												"phoneDoctor" => $row['phoneDoctor'],
+												"emailDoctor" => $row['emailDoctor'],
+												"dateExamination" => $row['dateExamination']));
+				} while($row = $result->fetch_assoc());
+				echo json_encode($userArray);
 			} else {
 				printError($mysqli->error); 
 			}
 			break;
 
 		case 3:
-			$editPacients = 'UPDATE user_info SET WHERE id_user = '.$_POST['idUser'];
+			$info = $_POST['info'];
+			$editPacients = "UPDATE user_info SET 
+								surname_user = '".$info['surname']."',
+								name_user = '".$info['name']."',
+								last_name_user = '".$info['lastName']."',
+								date_birthday = '".$info['dateBirthday']."',
+								gender = '".$info['gender']."',
+								email = '".$info['email']."',
+								phone = '".$info['phone']."',
+								address = '".$info['address']."',
+								region = '".$info['region']."',
+								heredity = '".$info['heredity']."',
+								smoky = '".$info['smoky']."',
+								work = '".$info['work']."',
+								alergos1 = '".$info['alergo1']."',
+								alergoSeason1 = '".$info['alergoSeason1']."',
+								alergoYear1 = '".$info['alergoYear1']."',
+								alergos2 = '".$info['alergo2']."',
+								alergoSeason2 = '".$info['alergoSeason2']."',
+								alergoYear2 = '".$info['alergoYear2']."',
+								alergos3 = '".$info['alergo3']."',
+								alergoSeason3 = '".$info['alergoSeason3']."',
+								alergoYear3 = '".$info['alergoYear3']."',
+								alergos4 = '".$info['alergo4']."',
+								alergos5 = '".$info['alergo5']."',
+								alergos6 = '".$info['alergo6']."',
+								alergos7 = '".$info['alergo7']."',
+								alergos8 = '".$info['alergo8']."',
+								alergos9 = '".$info['alergo9']."',
+								alergos10 = '".$info['alergo10']."',
+								hospital = '".$info['hospital']."',
+								surnameDoctor = '".$info['surnameDoctor']."',
+								phoneDoctor = '".$info['phoneDoctor']."',
+								emailDoctor = '".$info['emailDoctor']."',
+								dateExamination = '".$info['dateExamination']."',
+								checkItem = '".$info['checkItem']."'
+								WHERE id_user = ".$info['idUser'];
+
 			if($mysqli->query($editPacients)) {
-				echo json_encode(array("msg" => "Запис оновлено!"));
+				echo json_encode("data update!");
 			} else {
 				printError($mysqli->error); 
 			}
 			break;
 	}
-
-
 
 	$mysqli->close();	
 ?>
